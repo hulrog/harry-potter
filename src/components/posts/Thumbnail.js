e6d2dbb19efa662a3698aa106df56fa981c0a4cd
Hulrog <petar99t@gmail.com>
2023-07-07 14:08:26 +0200

    handleri za lajk, dislike, save, i boje

--- src/components/posts/Thumbnail.js
+++ src/components/posts/Thumbnail.js
@@ -1,8 +1,60 @@
-import React from "react";
-import { AiFillLike, AiFillDislike, AiOutlineComment } from "react-icons/ai";
+import React, { useState } from "react";
+import {
+  AiFillLike,
+  AiFillDislike,
+  AiOutlineComment,
+  AiFillPushpin,
+} from "react-icons/ai";
 import classes from "./Thumbnail.module.css";
 
 function Thumbnail({ post }) {
+  // Lajkovi i dislajkovi sa thumbnail-a
+  const [likes, setLikes] = useState(post.likes);
+  const [dislikes, setDislikes] = useState(post.dislikes);
+  // Moze biti liked i disliked
+  const [likeDislikeStatus, setLikeDislikeStatus] = useState("");
+  const [saved, setSaved] = useState(false);
+
+  const handleLike = () => {
+    if (likeDislikeStatus === "liked") {
+      setLikes(likes - 1);
+      setLikeDislikeStatus("");
+      // TODO unlike api
+    } else if (likeDislikeStatus === "disliked") {
+      setLikes(likes + 1);
+      setDislikes(dislikes - 1);
+      setLikeDislikeStatus("liked");
+      // TODO like api
+      // TODO undislike api
+    } else {
+      setLikes(likes + 1);
+      setLikeDislikeStatus("liked");
+      // TODO like api
+    }
+  };
+
+  const handleDislike = () => {
+    if (likeDislikeStatus === "disliked") {
+      setDislikes(dislikes - 1);
+      setLikeDislikeStatus("");
+      // TODO undislike api
+    } else if (likeDislikeStatus === "liked") {
+      setDislikes(dislikes + 1);
+      setLikes(likes - 1);
+      setLikeDislikeStatus("disliked");
+      // TODO dislike api
+      // TODO unlike api
+    } else {
+      setDislikes(dislikes + 1);
+      setLikeDislikeStatus("disliked");
+      // TODO dislike api
+    }
+  };
+
+  const handleSave = () => {
+    setSaved(!saved);
+  };
+
   // skracuje prikaz na prvih 400 karaktera
   const truncatedContent =
     post.content.length > 400
@@ -19,17 +71,33 @@ function Thumbnail({ post }) {
         <p className={classes.content}>{truncatedContent}</p>
       </div>
       <div className={classes.statsSection}>
-        <span className={classes.likes}>
+        <span
+          className={`${classes.likes} ${
+            likeDislikeStatus === "liked" && classes.liked
+          }`}
+          onClick={handleLike}
+        >
           <AiFillLike className={classes.icon} />
-          <span className={classes.number}>{post.likes}</span>
+          <span className={classes.number}>{likes}</span>
         </span>
-        <span className={classes.dislikes}>
+        <span
+          className={`${classes.dislikes} ${
+            likeDislikeStatus === "disliked" && classes.disliked
+          }`}
+          onClick={handleDislike}
+        >
           <AiFillDislike className={classes.icon} />
-          <span className={classes.number}>{post.dislikes}</span>
+          <span className={classes.number}>{dislikes}</span>
         </span>
         <span className={classes.comments}>
           <AiOutlineComment className={classes.icon} />
           <span className={classes.number}>{post.comments.length}</span>
+        </span>
+        <span
+          className={`${classes.save} ${saved && classes.saved}`}
+          onClick={handleSave}
+        >
+          <AiFillPushpin className={classes.icon} />
         </span>
       </div>
       <div className={classes.dateTimeSection}>
