1a0b6019ecfd73db3c65de5cbd313fcc0a62d1c7
Hulrog <petar99t@gmail.com>
2023-07-31 15:48:11 +0200

    tabovi za postove

--- src/components/posts/Feed.js
+++ src/components/posts/Feed.js
@@ -4,8 +4,10 @@ import Pagination from "./Pagination";
 import Thumbnail from "./Thumbnail";
 
 function Feed() {
-  //TODO - get post-ova preko API-ja
-  const postsData = [
+  const [selectedOption, setSelectedOption] = useState("for_you"); // Default state
+
+  //TODO - poziv za svaki API na useEffect - get post-ova preko API-ja
+  const forYouPostsData = [
     {
       id: 1,
       category: "Hogwarts",
@@ -248,6 +250,264 @@ function Feed() {
       ],
     },
   ];
+  const likedPostsData = [
+    {
+      id: 8,
+      category: "J.K. Rowling",
+      content: "Trying out a new recipe today.",
+      date: "2023-06-28",
+      dislikes: 2,
+      disliked: true,
+      house: "Hufflepuff",
+      likes: 14,
+      liked: false,
+      popularity: 100,
+      saved: true,
+      time: "15:45:00",
+      title: "Title of the Post",
+      user: "Grace Anderson",
+      user_id: 2,
+      awards: [],
+      comments: [
+        {
+          comment_id: 1,
+          user: "William Turner",
+          user_id: 500,
+          text: "Letme know how it turns out!",
+        },
+      ],
+    },
+  ];
+  const savedPostsData = [
+    {
+      id: 5,
+      category: "Hogwarts",
+      content: "Throwback to my amazing vacation.",
+      date: "2023-06-22",
+      dislikes: 0,
+      disliked: true,
+      house: "Slytherin",
+      likes: 20,
+      liked: false,
+      popularity: 180,
+      saved: true,
+      time: "16:00:00",
+      title: "Title of the Post",
+      user: "Sophia Clark",
+      user_id: 52,
+      awards: [{ name: "Travel Inspiration" }],
+      comments: [
+        {
+          comment_id: 1,
+          user: "Oliver Harris",
+          user_id: 66,
+          text: "Looks like a dream destination!",
+        },
+        {
+          comment_id: 2,
+          user: "Emma Davis",
+          user_id: 63,
+          text: "I wish I could go there too.",
+        },
+      ],
+    },
+    {
+      id: 6,
+      category: "Fanfic",
+      content: `Hi my name is Ebony Dark'ness Dementia Raven Way and I have long ebony black hair (that's how I got my name) 
+      with purple streaks and red tips that reaches my mid-back and icy blue eyes like limpid tears and a lot of people 
+      tell me I look like Amy Lee (AN: if u don't know who she is get da hell out of here!). I'm not related to 
+      Gerard Way but I wish I was because he's a major fucking hottie. I'm a vampire but my teeth are straight 
+      and white. I have pale white skin. I'm also a witch, and I go to a magic school called Hogwarts in England
+      where I'm in the seventh year (I'm seventeen). I'm a goth (in case you couldn't tell) and I wear mostly black.
+      I love Hot Topic and I buy all my clothes from there. For example today I was wearing a black corset with matching 
+      lace around it and a black leather miniskirt, pink fishnets and black combat boots. I was wearing black lipstick,
+      white foundation, black eyeliner and red eye shadow. I was walking outside Hogwarts. It was snowing and raining 
+      so there was no sun, which I was very happy about. A lot of preps stared at me. I put up my middle finger at them.`,
+      date: "2023-06-24",
+      dislikes: 323,
+      disliked: true,
+      house: "Slytherin",
+      likes: 20,
+      liked: false,
+      popularity: 180,
+      saved: true,
+      time: "20:30:00",
+      title: "My Immortal",
+      user: "Daniel Thompson",
+      user_id: 323,
+
+      awards: [],
+      comments: [
+        {
+          comment_id: 1,
+          user: "Lily Turner",
+          user_id: 2,
+          text: "Which band is performing?",
+        },
+      ],
+    },
+    {
+      id: 7,
+      category: "TV Show",
+      content: "Thoughts on the latest movie release?",
+      date: "2023-06-26",
+      dislikes: 4,
+      disliked: true,
+      house: "Hogwarts",
+      likes: 20,
+      liked: false,
+      popularity: 180,
+      saved: true,
+      time: "12:10:00",
+      title: "Title of the Post",
+      user: "Olivia Green",
+      user_id: 5,
+      awards: [{ name: "Movie Buff" }],
+      comments: [
+        {
+          comment_id: 1,
+          user: "Noah Martinez",
+          user_id: 1,
+          text: "I loved it! Great storyline.",
+        },
+        {
+          comment_id: 2,
+          user: "Ava Rodriguez",
+          user_id: 3,
+          text: "The cinematography was stunning.",
+        },
+        {
+          comment_id: 3,
+          user: "Ethan Hill",
+          user_id: 2,
+          text: "I was expecting more from it.",
+        },
+      ],
+    },
+    {
+      id: 8,
+      category: "J.K. Rowling",
+      content: "Trying out a new recipe today.",
+      date: "2023-06-28",
+      dislikes: 2,
+      disliked: true,
+      house: "Hufflepuff",
+      likes: 14,
+      liked: false,
+      popularity: 100,
+      saved: true,
+      time: "15:45:00",
+      title: "Title of the Post",
+      user: "Grace Anderson",
+      user_id: 2,
+      awards: [],
+      comments: [
+        {
+          comment_id: 1,
+          user: "William Turner",
+          user_id: 500,
+          text: "Letme know how it turns out!",
+        },
+      ],
+    },
+  ];
+  const commentedPostsData = [
+    {
+      id: 5,
+      category: "Hogwarts",
+      content: "Throwback to my amazing vacation.",
+      date: "2023-06-22",
+      dislikes: 0,
+      disliked: true,
+      house: "Slytherin",
+      likes: 20,
+      liked: false,
+      popularity: 180,
+      saved: true,
+      time: "16:00:00",
+      title: "Title of the Post",
+      user: "Sophia Clark",
+      user_id: 52,
+      awards: [{ name: "Travel Inspiration" }],
+      comments: [
+        {
+          comment_id: 1,
+          user: "Oliver Harris",
+          user_id: 66,
+          text: "Looks like a dream destination!",
+        },
+        {
+          comment_id: 2,
+          user: "Emma Davis",
+          user_id: 63,
+          text: "I wish I could go there too.",
+        },
+      ],
+    },
+    {
+      id: 6,
+      category: "Fanfic",
+      content: `Hi my name is Ebony Dark'ness Dementia Raven Way and I have long ebony black hair (that's how I got my name) 
+      with purple streaks and red tips that reaches my mid-back and icy blue eyes like limpid tears and a lot of people 
+      tell me I look like Amy Lee (AN: if u don't know who she is get da hell out of here!). I'm not related to 
+      Gerard Way but I wish I was because he's a major fucking hottie. I'm a vampire but my teeth are straight 
+      and white. I have pale white skin. I'm also a witch, and I go to a magic school called Hogwarts in England
+      where I'm in the seventh year (I'm seventeen). I'm a goth (in case you couldn't tell) and I wear mostly black.
+      I love Hot Topic and I buy all my clothes from there. For example today I was wearing a black corset with matching 
+      lace around it and a black leather miniskirt, pink fishnets and black combat boots. I was wearing black lipstick,
+      white foundation, black eyeliner and red eye shadow. I was walking outside Hogwarts. It was snowing and raining 
+      so there was no sun, which I was very happy about. A lot of preps stared at me. I put up my middle finger at them.`,
+      date: "2023-06-24",
+      dislikes: 323,
+      disliked: true,
+      house: "Slytherin",
+      likes: 20,
+      liked: false,
+      popularity: 180,
+      saved: true,
+      time: "20:30:00",
+      title: "My Immortal",
+      user: "Daniel Thompson",
+      user_id: 323,
+
+      awards: [],
+      comments: [
+        {
+          comment_id: 1,
+          user: "Lily Turner",
+          user_id: 2,
+          text: "Which band is performing?",
+        },
+      ],
+    },
+    {
+      id: 8,
+      category: "J.K. Rowling",
+      content: "Trying out a new recipe today.",
+      date: "2023-06-28",
+      dislikes: 2,
+      disliked: true,
+      house: "Hufflepuff",
+      likes: 14,
+      liked: false,
+      popularity: 100,
+      saved: true,
+      time: "15:45:00",
+      title: "Title of the Post",
+      user: "Grace Anderson",
+      user_id: 2,
+      awards: [],
+      comments: [
+        {
+          comment_id: 1,
+          user: "William Turner",
+          user_id: 500,
+          text: "Letme know how it turns out!",
+        },
+      ],
+    },
+  ];
 
   // Trenutno prikazana stranica
   const [currentPage, setCurrentPage] = useState(1);
@@ -258,7 +518,23 @@ function Feed() {
   // Slice-uje njih iz niza
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = postsData.slice(indexOfFirstPost, indexOfLastPost);
+
+  //Koji postovi ce se prikazati - for_you, liked, saved, ili commented
+  let currentPosts = [];
+  let totalPosts;
+  if (selectedOption === "for_you") {
+    totalPosts = forYouPostsData.length;
+    currentPosts = forYouPostsData.slice(indexOfFirstPost, indexOfLastPost);
+  } else if (selectedOption === "liked") {
+    currentPosts = likedPostsData.slice(indexOfFirstPost, indexOfLastPost);
+    totalPosts = likedPostsData.length;
+  } else if (selectedOption === "saved") {
+    currentPosts = savedPostsData.slice(indexOfFirstPost, indexOfLastPost);
+    totalPosts = savedPostsData.length;
+  } else if (selectedOption === "commented") {
+    currentPosts = commentedPostsData.slice(indexOfFirstPost, indexOfLastPost);
+    totalPosts = commentedPostsData.length;
+  }
 
   // Funkcija za promenu stranice
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
@@ -266,9 +542,43 @@ function Feed() {
   return (
     // Mapira postove koje dobije iz API-ja u Thumbnail-ove
     <div className={classes.feedContainer}>
+      <div className={classes.tabsContainer}>
+        <div
+          className={`${classes.tab} ${
+            selectedOption === "for_you" ? classes.activeTab : ""
+          }`}
+          onClick={() => setSelectedOption("for_you")}
+        >
+          For You
+        </div>
+        <div
+          className={`${classes.tab} ${
+            selectedOption === "liked" ? classes.activeTab : ""
+          }`}
+          onClick={() => setSelectedOption("liked")}
+        >
+          Liked
+        </div>
+        <div
+          className={`${classes.tab} ${
+            selectedOption === "saved" ? classes.activeTab : ""
+          }`}
+          onClick={() => setSelectedOption("saved")}
+        >
+          Saved
+        </div>
+        <div
+          className={`${classes.tab} ${
+            selectedOption === "commented" ? classes.activeTab : ""
+          }`}
+          onClick={() => setSelectedOption("commented")}
+        >
+          Commented
+        </div>
+      </div>
       <Pagination
         postsPerPage={postsPerPage}
-        totalPosts={postsData.length}
+        totalPosts={totalPosts}
         currentPage={currentPage}
         paginate={paginate}
       />
@@ -277,7 +587,7 @@ function Feed() {
       ))}
       <Pagination
         postsPerPage={postsPerPage}
-        totalPosts={postsData.length}
+        totalPosts={totalPosts}
         currentPage={currentPage}
         paginate={paginate}
       />
