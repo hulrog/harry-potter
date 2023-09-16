07f544f9d4f68713fb50fa8e6641294532f5015c
Hulrog <petar99t@gmail.com>
2023-09-16 22:29:29 +0200

    admin change

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -1,13 +1,20 @@
 import Categories from "../admin/Categories";
 import Credits from "../admin/Credits";
 import Students from "../admin/Students";
+import { useAuth } from "../auth/AuthContext";
 import classes from "./Admin.module.css";
 
 function AdminPage() {
+  const { currentUser } = useAuth();
+
   return (
     <div className={classes.adminContainer}>
-      <Categories></Categories>
-      <Students></Students>
+      {currentUser.role === "admin" && (
+        <>
+          <Categories></Categories>
+          <Students></Students>
+        </>
+      )}
       <Credits></Credits>
     </div>
   );
